926e9967e1824dd2fb89782888da88a6deb2e310
mnmanumek <mariusz.domitp@gmail.com>
2024-02-03 12:00:21 -0600

    add basic file

---
+++ basic.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+    //fill in desired code here
+    /*
+        comments could be more than one line
+        by starting with slash-star and ending with star-slash
+    */
+    return 0;
+}
