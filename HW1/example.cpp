41cbd1c7555d9c4b041d55fe7ee8e45c64e84bfc
mnmanumek <mariusz.domitp@gmail.com>
2024-02-03 11:25:06 -0600

    first_commit

---
+++ HW1/example.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Welcome to C++ Ilonka!!!" << endl;
+    return 0;
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # CSC121
 CSC121 W01 - COMPUTER SCIENCE I (Spring 2024 Credit Courses)
+my first c++
